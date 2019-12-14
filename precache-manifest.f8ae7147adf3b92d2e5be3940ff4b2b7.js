d5aa3689a553139a769573bbee3ed86fbf0922e0
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2019-12-14 21:32:35 +0200

    Updates

---
+++ precache-manifest.f8ae7147adf3b92d2e5be3940ff4b2b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4e278260256cc5d22a1902094d525f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e2c8422ae2a73a3b251",
+    "url": "./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "58612aa32a3e52f6c532",
+    "url": "./static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "0e2c8422ae2a73a3b251",
+    "url": "./static/js/2.d8de25da.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "./static/js/2.d8de25da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58612aa32a3e52f6c532",
+    "url": "./static/js/main.b8abb437.chunk.js"
+  },
+  {
+    "revision": "69dc833e55f44f930bc7",
+    "url": "./static/js/runtime-main.59014bcb.js"
+  }
+]);
